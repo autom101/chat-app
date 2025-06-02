95612c0abe5e7cbf9cde897c00cb2f7edcb1e7d9
autom101 <122489814+autom101@users.noreply.github.com>
2025-06-01 20:18:17 -0600

    Add a logger object to the WebSocketEventListener

--- src/main/java/com/sameer/chat/config/WebSocketEventListener.java
+++ src/main/java/com/sameer/chat/config/WebSocketEventListener.java
@@ -1,7 +1,15 @@
 package com.sameer.chat.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.logging.Logger;
 
 @Component
+@RequiredArgsConstructor
 public class WebSocketEventListener {
+    private static final Logger logger = Logger.getLogger(String.valueOf(WebSocketEventListener.class));
 }
